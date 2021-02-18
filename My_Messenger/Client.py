3d0918fdef6647394c8320ca22954cc0909ed78b
Gera <gera-60498@mail.ru>
2021-02-18 19:05:20 +0300

    HomeWork5
    Hw5

--- My_Messenger/Client.py
+++ My_Messenger/Client.py
@@ -1,8 +1,13 @@
 import argparse
+import sys
 import time
-from socket import socket, AF_INET, SOCK_STREAM
+import logging
+import log.Client.client_log_config
 
-from common.utils import send_message, get_data_from_message
+from socket import socket, AF_INET, SOCK_STREAM
+from common.utils import send_message, get_data_from_message, load_setting
+
+logger = logging.getLogger('client')
 
 
 def presence(sock):
@@ -16,18 +21,43 @@ def presence(sock):
         }
     }
     send_message(sock, msg_presence)
+    try:
+        response = sock.recv(1000000)
+    except Exception:
+        logger.exception('Ошибка при приеме ответа с сервера')
+        sys.exit(1)
+    return get_data_from_message(response)
 
-    data = get_data_from_message(sock.recv(1000000))
-    print('Сообщение от сервера: ', data)
+
+def main():
+    SETTINGS = load_setting(is_server=False)
+    parser = argparse.ArgumentParser(description='Client arguments')
+    parser.add_argument('addr', type=str, nargs='*', default='', help='Server address')
+    parser.add_argument('port', type=int, nargs='*', default='', help='server port')
+    args = parser.parse_args()
+
+    if not args.addr:
+        server_addr = SETTINGS["DEFAULT_IP_ADDRESS"]
+        logger.warning("Успользуются адрес сервера по умолчанию")
+    else:
+        server_addr = args.addr
+
+    if not args.port:
+        server_port = SETTINGS["DEFAULT_PORT"]
+        logger.warning("Успользуются порт сервера по умолчанию")
+    else:
+        server_port = args.port
+
+    s = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
+    try:
+        s.connect((server_addr, server_port))  # Соединиться с сервером
+    except ConnectionRefusedError:
+        logger.critical('Ошибка при подключении к серверу')
+        sys.exit(1)
+    response = presence(s)
+    logger.debug('Сообщение от сервера: ', response)
+    s.close()
 
 
-parser = argparse.ArgumentParser(description='Client arguments')
-parser.add_argument('addr', type=str, help='Server address')
-parser.add_argument('port', type=int, nargs='*', default=7777, help='server port')
-args = parser.parse_args()
-
-s = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
-s.connect((args.addr, args.port))  # Соединиться с сервером
-presence(s)
-
-s.close()
+if __name__ == '__main__':
+    main()
